52aab28629b29ea9e11ed4173e454e600c4ad2e8
Luke Bemish <lukebemish@lukebemish.dev>
2024-05-23 20:02:02 -0600

    Add javadoc

--- src/main/java/org/groovymc/rootpackagetransformer/RootPackage.java
+++ src/main/java/org/groovymc/rootpackagetransformer/RootPackage.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Classes marked with this annotation will be moved to the root package, alongside any string references to the class,
+ * when the transformer is applied to the compiled class files.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface RootPackage {
